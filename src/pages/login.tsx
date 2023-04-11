8c0229dcc90ceb268c5f657462fef023207fa940
Sannoob3144 <cotwo0139@gmail.com>
2023-04-11 15:17:22 +0900

    feat(package.json): add next-auth, add fontawesome

--- src/pages/login.tsx
+++
@@ -1,7 +0,0 @@
-export default function DiscordLogin() {
-    return (
-        <div>
-            <a href="https://discord.com/api/oauth2/authorize?client_id=426722888293548032&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fcallback&response_type=code&scope=identify%20email%20guilds">Login with Discord</a>
-        </div>
-    )
-}
