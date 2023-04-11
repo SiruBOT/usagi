f1597e42c3d08038c9f400f0bb6f683fb66ecb0c
Sannoob3144 <cotwo0139@gmail.com>
2023-04-11 18:58:33 +0900

    feat(/api/auth/[...nextauth]): add next-auth endpoint feat(_app): add SessionProvider

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,72 @@
+import { AuthOptions, DefaultSession } from "next-auth";
+import NextAuth from "next-auth/next";
+import DiscordProvider from "next-auth/providers/discord";
+
+interface DiscordUser {
+    id: string;
+    username: string;
+    avatar: string;
+    discriminator: string;
+    locale: string;
+    mfa_enabled: boolean;
+    email: string;
+    verified: boolean;
+    premium_type: number;
+}
+
+declare module "next-auth" {
+  interface User {}
+
+  interface Session extends DefaultSession {
+    accessToken: string;
+    discordInfo: DiscordUser;
+  }
+}
+
+export const authOptions: AuthOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+      authorization: { params: { scope: "identify guilds email" } },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      if (token.accessToken) {
+        const resp = await fetch("https://discord.com/api/users/@me", {
+          headers: {
+            authorization: `Bearer ${token.accessToken}`,
+          },
+        }).then((e) => e.json());
+        session.discordInfo = {
+            id: resp.id,
+            username: resp.username,
+            avatar: resp.avatar,
+            discriminator: resp.discriminator,
+            locale: resp.locale,
+            mfa_enabled: resp.mfa_enabled,
+            email: resp.email,
+            verified: resp.verified,
+            premium_type: resp.premium_type,
+        }
+      }
+      return session;
+    },
+    async jwt({ token, user, account }) {
+      if (user) {
+        token.id = user.id;
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+  },
+  pages: {
+    error: "/",
+  },
+};
+
+export default NextAuth(authOptions);
