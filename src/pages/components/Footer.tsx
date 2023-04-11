c4191089f180b05b0c03f05828435db208770940
Sannoob3144 <cotwo0139@gmail.com>
2023-04-11 19:13:08 +0900

    feat(Footer): add footer

---
+++ src/pages/components/Footer.tsx
@@ -0,0 +1,15 @@
+import styles from "./Footer.module.css";
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <footer className={[styles.footer, "center"].join(" ")}>
+      <Image
+        src="/next.svg"
+        alt={"vercel logo"}
+        width="128"
+        height="512"
+      ></Image>
+    </footer>
+  );
+}
