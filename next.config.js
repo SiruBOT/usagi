4f70e6d53dc9515806e4a6e2a6b38ee3c786acce
Sannoob3144 <cotwo0139@gmail.com>
2023-04-14 00:06:51 +0900

    feat(/stats): add bot stats page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'cdn.discordapp.com',
+      port: '',
+      pathname: '/**/**',
+    }]
+  },
   reactStrictMode: true,
 }
 
--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -1,10 +1,51 @@
 import Header from "./components/Header";
+import ShardInfo from "./components/ShardInfo";
+import { ApiResponse } from "./types/StatsApiResponse";
+import styles from "./stats.module.css";
+import Divider from "./components/Divider";
 
-export default function Stats() {
+export default function Stats({ stats }: { stats: ApiResponse }) {
+  const totalGuildCount = stats.clustersInfo.reduce((acc, cur) => {
+    acc += cur.discordStats.cachedGuilds;
+    return acc;
+  }, 0);
+  const totalChannelCount = stats.clustersInfo.reduce((acc, cur) => {
+    acc += cur.discordStats.cachedChannels;
+    return acc;
+  }, 0);
+  const totalUserCount = stats.clustersInfo.reduce((acc, cur) => {
+    acc += cur.discordStats.cachedUsers;
+    return acc;
+  }, 0);
   return (
     <>
-      <Header title="Stats" />
-      <a>Stats</a>
+      <Header title="봇 정보" />
+      <h1 className={styles.titleTypo}>클러스터 정보</h1>
+
+      <ul className={styles.totalContainer}>
+        <li>{totalGuildCount}</li>
+        <Divider orientation="vertical" />
+        <li>{totalChannelCount}</li>
+        <Divider orientation="vertical" />
+        <li>{totalUserCount}</li>
+      </ul>
+      <div className={styles.shardContainer}>
+        {stats.clustersInfo.map((info, index) => {
+          return <ShardInfo key={index} clusterInfo={info} />;
+        })}
+      </div>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3300/stats");
+  const data = await res.json();
+
+  return {
+    props: {
+      stats: data,
+      revalidate: 10,
+    },
+  };
 }
