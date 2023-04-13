9c3f5fcda98dea4f198b5113caeba6d50ed65c67
Sannoob3144 <cotwo0139@gmail.com>
2023-04-14 00:10:10 +0900

    feat(Layout): add footer to layout

--- src/pages/components/Layout.tsx
+++ src/pages/components/Layout.tsx
@@ -1,10 +1,12 @@
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }: React.PropsWithChildren<{}>) {
     return (
         <>
         <Navbar />
-        <div className="container">{children}</div>
+        <div className="container center">{children}</div>
+        <Footer />
         </>
     );
 }
