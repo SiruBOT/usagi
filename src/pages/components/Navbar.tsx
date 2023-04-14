480e73c059cf1db9fea45bace4f817ed9271208b
Sannoob3144 <cotwo0139@gmail.com>
2023-04-14 09:03:13 +0900

    feat(Navbar): move logo to left, add dashboard button, add login, logout button, responsive design

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,27 +1,44 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./Navbar.module.css";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Navbar() {
+  const { data, status } = useSession();
   return (
-    <div className={styles.navbar}>
-        <Link href="/" className={`${styles.align} ${styles.button} ${styles.logoHref}`}>
-          <Image
-            src="/images/logo.png"
-            alt="Logo"
-            width={100}
-            height={100}
-            className={styles.logo}
-          />
-          <h1>치노봇</h1>
-        </Link>
+    <div className={[styles.navbar, "center"].join(" ")}>
       <div className={styles.align}>
-        <Link href="/stats" className={styles.button}>
-          치노봇 샤드 상태
+      <Link
+        href="/"
+        className={`${styles.align} ${styles.logoButton}`}
+      >
+        <Image
+          src="/images/logo.png"
+          alt="Logo"
+          width={100}
+          height={100}
+          className={styles.logo}
+        />
+        <h1>치노봇</h1>
+      </Link>
+      <Link href="/stats" className={styles.button}>
+          봇 상태
         </Link>
-        <Link href="/login" className={styles.button}>
-          로그인
+      </div>
+      <div className={styles.align}>
+      <Link href="/dashboard" className={styles.button}>
+          대시보드
         </Link>
+        {status == "authenticated" ? (
+          <button onClick={() => signOut()} className={styles.button}>{`${data.user?.name}#${data.discordInfo.discriminator}`}</button>
+        ) : (
+          <button
+            onClick={() => signIn("discord")}
+            className={styles.loginButton}
+          >
+            <a>디스코드로 로그인</a>
+          </button>
+        )}
       </div>
     </div>
   );
