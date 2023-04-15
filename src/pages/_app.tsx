6116e7bdaaa1f2c6b534797fee15cbe6759d9bc9
Sannoob3144 <cotwo0139@gmail.com>
2023-04-16 04:03:58 +0900

    feat(_app.tsx): add @tanstack/react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,27 @@
 import { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import Layout from "./components/Layout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Layout from "@components/Layout";
 import "@/styles/globals.css";
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <QueryClientProvider client={client}>
+      {process.env.NODE_ENV !== "production" ? <ReactQueryDevtools initialIsOpen={true}/>  : null}
+      <SessionProvider session={pageProps.session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </QueryClientProvider>
   );
 }
