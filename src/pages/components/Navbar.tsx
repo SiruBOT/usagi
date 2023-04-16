9e0787c5c34c705469ec673b739f4c0cd78902a4
Sannoob3144 <cotwo0139@gmail.com>
2023-04-16 23:31:54 +0900

    feat(Navbar): add loading status

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,45 +1,55 @@
 import Link from "next/link";
 import Image from "next/image";
-import styles from "./Navbar.module.css";
+import styles from "@components/Navbar.module.css";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
   const { data, status } = useSession();
+  const router = useRouter();
   return (
-    <div className={[styles.navbar, "center"].join(" ")}>
+    <nav className={[styles.navbar, "center"].join(" ")}>
       <div className={styles.align}>
-      <Link
-        href="/"
-        className={`${styles.align} ${styles.logoButton}`}
-      >
-        <Image
-          src="/images/logo.png"
-          alt="Logo"
-          width={100}
-          height={100}
-          className={styles.logo}
-        />
-        <h1>치노봇</h1>
-      </Link>
+        <Link href="/" className={`${styles.align} ${styles.logoButton}`}>
+          <Image
+            src="/images/logo.png"
+            alt="Logo"
+            width={100}
+            height={100}
+            className={styles.logo}
+          />
+          <h1>치노봇</h1>
+        </Link>
+      </div>
+      <div className={styles.align}>
       <Link href="/stats" className={styles.button}>
           봇 상태
         </Link>
-      </div>
-      <div className={styles.align}>
-      <Link href="/dashboard" className={styles.button}>
+        <Link href="/dashboard" className={styles.button}>
           대시보드
         </Link>
-        {status == "authenticated" ? (
-          <button onClick={() => signOut()} className={styles.button}>{`${data.user?.name}#${data.discordInfo.discriminator}`}</button>
-        ) : (
-          <button
-            onClick={() => signIn("discord")}
-            className={styles.loginButton}
-          >
-            <a>디스코드로 로그인</a>
-          </button>
-        )}
+        <button
+          onClick={
+            status == "authenticated"
+              ? () => {
+                  router.push("/");
+                  signOut({ redirect: true, callbackUrl: "/" });
+                }
+              : () => signIn("discord")
+          }
+          className={
+            status == "loading" || status == "authenticated"
+              ? styles.button
+              : styles.loginButton
+          }
+        >
+          {status == "authenticated"
+            ? `${data?.user?.name}#${data?.discordInfo.discriminator}`
+            : status == "unauthenticated"
+            ? "디스코드로 로그인"
+            : "로딩중..."}
+        </button>
       </div>
-    </div>
+    </nav>
   );
 }
