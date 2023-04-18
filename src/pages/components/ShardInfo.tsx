d8c959f8c4fb5719a410810181a07ec2fe8b190b
Sannoob3144 <cotwo0139@gmail.com>
2023-04-18 23:50:56 +0900

    feat(ShardInfo): add shard info component

---
+++ src/pages/components/ShardInfo.tsx
@@ -0,0 +1,18 @@
+import { ClusterInfo } from "@/typings"
+import styles from "./ShardInfo.module.css";
+
+export default function ShardInfo({ clusterInfo }: { clusterInfo: ClusterInfo }) {
+  const {discordStats, websocketStatus, audioStats, clusterId } = clusterInfo;
+  return (
+    <div className={styles.card}>
+      <h1>Cluster Id {clusterInfo.clusterId}</h1>
+      <a>{discordStats.cachedChannels} Channels</a>
+      <a>{discordStats.cachedGuilds} Guilds</a>
+      <a>{discordStats.cachedUsers} Users</a>
+      <a>{websocketStatus.wsLatency}ms</a>
+      <a>{clusterId}</a>
+      <a>{audioStats.audioNodes[0]?.name}</a>
+      <a>{audioStats.audioDispatchers} Players</a>
+    </div>
+  );
+}
